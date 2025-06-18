e2ce0f82ce913397c5e0011dc181049077f7fb80
elisarrtech <146400511+elisarrtech@users.noreply.github.com>
2025-06-18 02:49:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -69,3 +69,8 @@ def get_bot_response():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=os.getenv('ENVIRONMENT') != 'PRODUCTION')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
